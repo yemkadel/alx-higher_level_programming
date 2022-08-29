81f7c9f085febc4b5e7fd4547d0871a3ced534dd
yemkadel <agosifeso@gmail.com>
2022-08-29 10:19:10 +0000

    tuple

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence:
+        if len(sentence) == 0:
+            firstChr = "None"
+        else:
+            firstChr = sentence[0]
+        return len(sentence), firstChr
