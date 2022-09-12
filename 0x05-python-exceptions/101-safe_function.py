4d60c875eb38b4ccad49f13fec9353810e984098
yemkadel <agosifeso@gmail.com>
2022-09-12 21:27:08 +0000

    py_exceptions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
+    else:
+        return result
