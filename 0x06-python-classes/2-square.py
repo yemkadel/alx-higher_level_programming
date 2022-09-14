e74e0f2e11eca7997b5e6d272ca0731d9eb7c3d7
yemkadel <agosifeso@gmail.com>
2022-09-14 15:06:40 +0000

    class

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+""" This is a Square File """
+
+
+class Square:
+    """ The is a Square class """
+
+    def __init__(self, size=0):
+        if (type(size) != int):
+            raise TypeError('size must be an integer')
+        elif (size < 0):
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
