c4beebd1b289d85fea5c7457edbd34de9167cb9f
yemkadel <agosifeso@gmail.com>
2022-09-14 15:39:18 +0000

    class

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" This is a Square File """
+
+
+class Square:
+    """ The is a Square class """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """ size getter method """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ size setter method """
+        if (type(value) != int):
+            raise TypeError('size must be an integer')
+        elif (value < 0):
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        """ This method calculates and returns the area
+        of a square.
+        Fomular is Lenght * breadth
+        """
+        return self.__size * self.__size
