c8d1be48603d56e5ca25fa0d08462e68a3653dcd
yemkadel <agosifeso@gmail.com>
2022-09-20 19:29:10 +0000

    more_classes

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" This a copy_list module """
+
+
+def copy_list(l):
+    """ 
+         the function returns a new copy of the list
+    """
+    new_l = l[:]
+    return new_l
