2ed33106c978c0624d6f157ff1147649fc249c17
yemkadel <agosifeso@gmail.com>
2022-09-28 23:35:53 +0000

    io

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" This Module defines a student class """
+
+
+class Student:
+    """ This class defines a student """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ this function returns a dict representation of a student """
+        if (type(attrs) == list and
+                all(type(item) == str for item in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
