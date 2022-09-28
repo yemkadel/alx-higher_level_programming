2a3316562a2d497244df469f6c09c936b7711da8
yemkadel <agosifeso@gmail.com>
2022-09-28 20:16:22 +0000

    io

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3i
+""" This module is about json to string. """
+import json
+
+
+def to_json_string(my_str):
+    """ This function returns a json representation of a string obj. """
+    return json.loads(my_str)
