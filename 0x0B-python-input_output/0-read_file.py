734add3a0d6275b5c68109ac4eb37a453139d748
yemkadel <agosifeso@gmail.com>
2022-09-28 16:36:37 +0000

    io

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
+    """ Print the contents of the file to stdout """
     with open(filename, encoding="utf-8") as f:
         for line in f:
             print(line, end="")
